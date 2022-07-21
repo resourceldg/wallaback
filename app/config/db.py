22681e5468269b4cb594c8a4a49609a7cee22779
resourceldg <32602897+resourceldg@users.noreply.github.com>
2022-07-21 02:08:17 -0300

    Update db.py

--- app/config/db.py
+++ app/config/db.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 #production mode
 #SQLALCHEMY_DATABASE_URL = "postgresql://username:password@db:5432/database"
 #dev mode
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://username:password@200.58.100.85:5432/database"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://username:password@66.97.36.28:5432/database"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 conn = engine.connect()
